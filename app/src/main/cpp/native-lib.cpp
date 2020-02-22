8f697e1155928d35af7a0724baa5f1f40dcbe1fc
boyangx <boyangx@umich.edu>
2020-02-22 15:04:22 -0500

    some small xml changes

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,6 +5,6 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_bestbiteapp_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Grab your Best Bite!";
     return env->NewStringUTF(hello.c_str());
 }
--- app/src/main/java/com/example/bestbiteapp/MainActivity.java
+++ app/src/main/java/com/example/bestbiteapp/MainActivity.java
@@ -1,25 +1,20 @@
 package com.example.bestbiteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+    // Example of a call to a native method
+    TextView tv = findViewById(R.id.sample_text);
+    tv.setText(stringFromJNI());
     }
 
     /**
@@ -27,4 +22,9 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
 }
