ac73dfb32ce47a8d0cb23d59e0fd465ebdd056fe
fedora <fedora@ns527193.ip-149-56-18.net>
2020-03-08 03:33:52 +0000

    debugging version

--- app/src/main/java/com/example/bestbiteapp/Preference1.java
+++ app/src/main/java/com/example/bestbiteapp/Preference1.java
@@ -1,15 +1,46 @@
 package com.example.bestbiteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class Preference1 extends AppCompatActivity {
+public class Preference1 extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    String[] options;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_preference1);
+        setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Creating ArrayAdapter using the string array and default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.Animals, android.R.layout.simple_spinner_item);
+        // Specify layout to be used when list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Applying the adapter to our spinner
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        options = Preference1.this.getResources().getStringArray(R.array.Animals);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        Toast.makeText(this, " You select >> "+options[position], Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
