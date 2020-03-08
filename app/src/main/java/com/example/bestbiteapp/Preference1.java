395d3aca39fdd80773660b9f251465bc8b91cf36
fedora <fedora@ns527193.ip-149-56-18.net>
2020-03-08 01:14:54 +0000

    add preference page

---
+++ app/src/main/java/com/example/bestbiteapp/Preference1.java
@@ -0,0 +1,14 @@
+package com.example.bestbiteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Preference1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preference1);
+    }
+}
