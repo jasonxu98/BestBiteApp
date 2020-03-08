b0326682bf9445a70962acab443eeb1af30267b8
fedora <fedora@ns527193.ip-149-56-18.net>
2020-03-08 01:59:21 +0000

    add scroll view

--- app/src/main/java/com/example/bestbiteapp/Preference1.java
+++ app/src/main/java/com/example/bestbiteapp/Preference1.java
@@ -10,5 +10,6 @@ public class Preference1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preference1);
+        getSupportActionBar().hide();
     }
 }
