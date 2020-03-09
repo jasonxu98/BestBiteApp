a3bdc6363b572704b2154f99d72614f303381079
fedora <fedora@ns527193.ip-149-56-18.net>
2020-03-09 02:21:57 +0000

    add user permission

--- app/src/main/java/com/example/bestbiteapp/Preference1.java
+++ app/src/main/java/com/example/bestbiteapp/Preference1.java
@@ -8,15 +8,27 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+
+
 public class Preference1 extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     String[] options;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preference1);
         getSupportActionBar().hide();
+
 
         Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
         // Creating ArrayAdapter using the string array and default spinner layout
